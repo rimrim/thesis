f4878bd417a05aa2935a9e8a7eeaf5798aea108a
rimrim <dinhquangrim@gmail.com>
2018-01-12 14:37:32 +1100

    Initial ShareLaTeX Import

---
+++ thesis/Chapter1/auto/chapter1.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "chapter1"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:biometricIntro"
+    "sec:privacyLiteratures"
+    "sec:thesisContributions"
+    "sec:introOrganize"))
+ :latex)
+
---
+++ thesis/auto/chapter4.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "chapter4"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (LaTeX-add-labels
+    "sec:secProcIntro"
+    "sec:secProcPrevious"
+    "sec:secProcRenyi"
+    "eq:renyi"
+    "le:Renyi"
+    "sec:2correctness"
+    "sec:secProcResult"))
+ :latex)
+
